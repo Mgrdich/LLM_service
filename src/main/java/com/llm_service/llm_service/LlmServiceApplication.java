10cc39346ccaf47f579198d76dfbbfa135f9e171
Mgrdich <mgotm13@gmail.com>
2024-05-03 12:10:05 +0400

    Initial commit

---
+++ src/main/java/com/llm_service/llm_service/LlmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.llm_service.llm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llm_service/llm_service/LlmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llm_service.llm_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
