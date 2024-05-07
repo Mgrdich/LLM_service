202d0bfbea63f56edbf5bff0b76d0d9def898e35
Mgrdich <mgotm13@gmail.com>
2024-05-08 01:51:17 +0400

    Fix the Routing + add tanstack query for fetching

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,30 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Welcome from "./pages/Welcome/Welcome.tsx";
+import Conversation from "./pages/Conversation/Conversation.tsx";
+import Login from "./pages/Auth/Login.tsx";
+import Register from "./pages/Auth/Register.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Welcome,
+  },
+  {
+    path: "/conversation",
+    Component: Conversation,
+  },
+  {
+    path: "/login",
+    Component: Login,
+  },
+  {
+    path: "/register",
+    Component: Register,
+  },
+]);
+
 function App() {
-  return <>Hello</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
