c3b32fd58e64a0c6c6063d37e03cf22383c74725
Yerish26 <yeghiaa@gmail.com>
2024-05-08 00:59:55 +0400

    make code spotless

--- backend/src/main/java/com/llm_service/llm_service/controller/discussion/DiscussionControllerMapper.java
+++ backend/src/main/java/com/llm_service/llm_service/controller/discussion/DiscussionControllerMapper.java
@@ -2,12 +2,12 @@ package com.llm_service.llm_service.controller.discussion;
 
 import com.llm_service.llm_service.model.Conversation;
 import com.llm_service.llm_service.model.Discussion;
-import org.mapstruct.Mapper;
-
 import java.util.UUID;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface DiscussionControllerMapper {
     DiscussionResponse map(Discussion Discussion);
+
     UUID map(Conversation value);
 }
