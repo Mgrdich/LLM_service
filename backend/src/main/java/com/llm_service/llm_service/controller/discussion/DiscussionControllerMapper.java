368642bd1d9db27e2b36dcc327ccc676c7d79d44
Yerish26 <yeghiaa@gmail.com>
2024-05-08 00:58:45 +0400

    add a mapper to get UUID of Conversation

--- backend/src/main/java/com/llm_service/llm_service/controller/discussion/DiscussionControllerMapper.java
+++ backend/src/main/java/com/llm_service/llm_service/controller/discussion/DiscussionControllerMapper.java
@@ -1,9 +1,13 @@
 package com.llm_service.llm_service.controller.discussion;
 
+import com.llm_service.llm_service.model.Conversation;
 import com.llm_service.llm_service.model.Discussion;
 import org.mapstruct.Mapper;
+
+import java.util.UUID;
 
 @Mapper(componentModel = "spring")
 public interface DiscussionControllerMapper {
     DiscussionResponse map(Discussion Discussion);
+    UUID map(Conversation value);
 }
