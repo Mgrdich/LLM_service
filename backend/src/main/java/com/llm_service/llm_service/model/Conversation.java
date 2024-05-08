bf7c15f99577c9028e3ec641d72723d5076a22f5
Yerish26 <yeghiaa@gmail.com>
2024-05-08 22:50:29 +0400

    added Base Model

--- backend/src/main/java/com/llm_service/llm_service/model/Conversation.java
+++ backend/src/main/java/com/llm_service/llm_service/model/Conversation.java
@@ -2,13 +2,13 @@ package com.llm_service.llm_service.model;
 
 import java.util.List;
 import java.util.UUID;
-import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
-@Value
-@Builder(toBuilder = true)
+@SuperBuilder
+@Data
 public class Conversation extends Base {
     List<UUID> discussions;
 }
--- backend/src/main/java/com/llm_service/llm_service/model/Discussion.java
+++ backend/src/main/java/com/llm_service/llm_service/model/Discussion.java
@@ -2,13 +2,13 @@ package com.llm_service.llm_service.model;
 
 import com.llm_service.llm_service.persistance.entities.DiscussionRole;
 import java.util.UUID;
-import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
-@Value
-@Builder(toBuilder = true)
+@SuperBuilder
+@Data
 public class Discussion extends Base {
     UUID id;
     String text;
