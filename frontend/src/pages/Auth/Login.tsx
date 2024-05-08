f4d6e7e91241377417a203d7f1ef616489e8c2dc
Rita <rita.bakkalian@gmail.com>
2024-05-08 22:45:01 +0400

    Resolved the comment

--- frontend/src/pages/Auth/Login.tsx
+++ frontend/src/pages/Auth/Login.tsx
@@ -9,25 +9,25 @@ function Login() {
           <div className="form-group mb-6">
             <label htmlFor="exampleInputEmail2" className="form-label inline-block mb-2 text-gray-700">
               Email
+              <input
+                type="email"
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                id="exampleInputEmail2"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
             </label>
-            <input
-              type="email"
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="exampleInputEmail2"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
           </div>
           <div className="form-group mb-6">
             <label htmlFor="exampleInputPassword2" className="form-label inline-block mb-2 text-gray-700">
               Password
+              <input
+                type="password"
+                className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                id="exampleInputPassword2"
+                placeholder="Password"
+              />
             </label>
-            <input
-              type="password"
-              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-              id="exampleInputPassword2"
-              placeholder="Password"
-            />
           </div>
           <div className="flex justify-between items-center mb-6">
             <div className="form-group form-check">
