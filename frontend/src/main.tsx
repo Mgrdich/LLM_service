d38d133f35db023c12563f342637695c7505551e
Rita <rita.bakkalian@gmail.com>
2024-05-08 22:29:07 +0400

    Tried fixing the lint warning

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
