f0c41700c36df235c440384a640c188a5b81c99f
Tigran Asatryan <t.asatryan96@gmail.com>
2024-05-09 00:48:11 +0400

    eslint config changes (#21)
    * eslint config changes
    
    * Fix the linting errors
    
    ---------
    
    Co-authored-by: Mgrdich <mgotm13@gmail.com>

--- frontend/src/ui/Checkbox.tsx
+++ frontend/src/ui/Checkbox.tsx
@@ -9,7 +9,9 @@ export default function Checkbox({ className, ...rest }: CheckboxProps) {
       {...rest}
       type="checkbox"
       className={twMerge(
-        "form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer",
+        "form-check-input appearance-none h-4 w-4 border border-gray-300" +
+          "rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none" +
+          " transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer",
         className,
       )}
     />
--- frontend/src/ui/Input.tsx
+++ frontend/src/ui/Input.tsx
@@ -9,7 +9,9 @@ export default function Input({ className, ...rest }: InputProps) {
       {...rest}
       type="text"
       className={twMerge(
-        "form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
+        "form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding" +
+          " border border-solid border-gray-300 rounded transition ease-in-out m-0 " +
+          "focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
         className,
       )}
     />
