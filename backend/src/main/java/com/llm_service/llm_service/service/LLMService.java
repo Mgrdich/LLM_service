9d77cb8ce11b1837776367a9c293f7a85dfcd6ff
joseph-alakach <joseph_alakach@edu.aua.am>
2024-05-10 20:14:58 +0400

    Llm enhancments (#37)
    * chore: Fix the workflow push logic (#3)
    
    * did some prompt engineering + prompt preprocessing
    
    * minor change
    
    * minot bug fix
    
    ---------
    
    Co-authored-by: Mgrdich <46796009+Mgrdich@users.noreply.github.com>
    Co-authored-by: joseph-alakach <joseph-alakach@edu.aua.am>

--- backend/src/main/java/com/llm_service/llm_service/service/LLMService.java
+++ backend/src/main/java/com/llm_service/llm_service/service/LLMService.java
@@ -1,9 +1,12 @@
 package com.llm_service.llm_service.service;
 
+import com.llm_service.llm_service.dto.Discussion;
+import com.llm_service.llm_service.persistance.entities.DiscussionRole;
 import de.kherud.llama.InferenceParameters;
 import de.kherud.llama.LlamaModel;
 import de.kherud.llama.ModelParameters;
 import java.io.File;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +14,15 @@ public class LLMService {
     private final ModelParameters modelParameters;
     private final LlamaModel llamaModel;
 
+    public static final String USERINST = " [/INST] ";
+    public static final String SYSTEMINST = " </s><s>[INST] ";
+
     final String prefix = "def remove_non_ascii(s: str) -> str:\n    \"\"\" ";
     final String suffix = "\n    return result\n";
 
     public LLMService() {
         modelParameters =
-                new ModelParameters().setModelFilePath("model" + File.separator + "model/llama-2-13b-chat.Q5_K_S.gguf");
+                new ModelParameters().setModelFilePath("model" + File.separator + "llama-2-13b-chat.Q5_K_S.gguf");
         llamaModel = new LlamaModel(modelParameters);
     }
 
@@ -30,5 +36,29 @@ public class LLMService {
         }
 
         return buffer;
+    }
+
+    public String generateFullResponse(String text) {
+        InferenceParameters inferParams = new InferenceParameters(text).setNPredict(128);
+        return llamaModel.complete(inferParams);
+    }
+
+    public String initializeModelSystem() {
+        return "<s>[INST] <<SYS>>\n"
+                + "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.\n"
+                + "<</SYS>>\n"
+                + " \n";
+    }
+
+    public String preprocessPrompt(List<Discussion> discussions) {
+        String messages = "";
+        for (Discussion discussion : discussions) {
+            if (discussion.getPromptRole() == DiscussionRole.USER) {
+                messages += discussion.getText() + USERINST;
+            } else {
+                messages += discussion.getText() + SYSTEMINST;
+            }
+        }
+        return messages;
     }
 }
