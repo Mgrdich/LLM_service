8c7e64e9e803218b3c1154b1225704dae54e886d
Rita <rita.bakkalian@gmail.com>
2024-05-10 18:05:28 +0400

    Styled the error pages, changed the button style and did a tiny change on the welcome page

--- frontend/src/pages/Error/Error404.tsx
+++ frontend/src/pages/Error/Error404.tsx
@@ -1,5 +1,10 @@
 import Error from "./Error.tsx";
 
 export default function Error404() {
-  return <Error>404 Page is not found</Error>;
+  return (
+    <div className="mb-12 ml-12 lg:mb-0 flex flex-col justify-center items-center h-screen pb-20 pt-20">
+      <img src="/error404.png" className="w-fit rounded-lg" alt="" width="300" height="300" />
+      <Error>Page Not Found</Error>
+    </div>
+  );
 }
--- frontend/src/pages/Error/Error500.tsx
+++ frontend/src/pages/Error/Error500.tsx
@@ -1,5 +1,10 @@
 import Error from "./Error.tsx";
 
 export default function Error500() {
-  return <Error>Something went wrong</Error>;
+  return (
+    <div className="mb-12 ml-12 lg:mb-0 flex flex-col justify-center items-center h-screen pb-20 pt-20">
+      <img src="/error500.png" className="w-fit rounded-lg" alt="" width="300" height="300" />
+      <Error />
+    </div>
+  );
 }
