27e450249cf807c837e6b529648094d9610a9ad0
Rita <rita.bakkalian@gmail.com>
2024-05-10 16:44:12 +0400

    Welcome page styling modifications

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,7 +10,35 @@ export default {
       },
       fontFamily: {
         'mono': ['Fira Code', 'monospace']
-      }
+      },
+      animation: {
+        typewriter: 'typewriter 2s steps(11) forwards',
+        caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
     },
   },
   plugins: [],
